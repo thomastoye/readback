5176454ee5b1084164c06d8b8884281d1ebb3b09
Thomas Toye <thomas@toye.io>
2022-10-05 20:51:28 +0200

    Initialize nx monorepo

---
+++ apps/readback-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'readback-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'readback-web';
+}
