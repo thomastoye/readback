c0046cde1b0e26b4a28942a13ef42925dbbf7e21
Thomas Toye <thomas@toye.io>
2022-10-06 12:06:17 +0200

    Add Tailwind

--- apps/readback-web/src/app/app.module.ts
+++ apps/readback-web/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/readback-web/src/app/services/ditto.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DittoService } from './ditto.service';
+
+describe('DittoService', () => {
+  let service: DittoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DittoService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
