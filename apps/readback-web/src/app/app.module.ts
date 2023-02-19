7d5d3adf4a564160fb013697ba5df0f492de877d
Thomas Toye <thomas@toye.io>
2023-02-19 16:47:40 +0100

    Add two basic components

--- apps/readback-web/src/app/app.module.ts
+++ apps/readback-web/src/app/app.module.ts
@@ -7,12 +7,27 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatListModule } from '@angular/material/list'
 
 import { AppComponent } from './components/app/app.component'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './components/home/home.component'
+import { UserComponent } from './components/user/user.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+]
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, UserComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     MatSidenavModule,
     MatIconModule,
     MatToolbarModule,
